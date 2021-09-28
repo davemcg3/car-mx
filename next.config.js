16771e2e1d41702f8ed2f7ae42c8fc2f8d79153e
davemcg3 <dave.mcgrath3@gmail.com>
2021-09-28 00:54:27 -0600

    Updated image optimization for static deployment, ie removed it so there is no more image optimization :(

--- next.config.js
+++ next.config.js
@@ -1,12 +1,35 @@
 // This file is not going through babel transformation.
 // So, we write it in vanilla JS
 // (But you could use ES2015 features supported by your Node.js version)
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
 
 const debug = process.env.NODE_ENV !== 'production'
 
-module.exports = {
-  assetPrefix: !debug ? '' : '',
-  images: {
-    domains: ["localhost"],
+const nextConfig = {
+  webpack: (config, options) => {
+    return config;
   },
-}
+  eslint: {
+    // ESLint managed on the workspace level
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    disableStaticImages: true,
+  },
+};
+
+const config = withPlugins(
+  [
+    [
+      optimizedImages,
+      {
+        // optimisation disabled by default, to enable check https://github.com/cyrilwanner/next-optimized-images
+        optimizeImages: false,
+      },
+    ],
+  ],
+  nextConfig
+);
+
+module.exports = config;
--- updates/20210927.js
+++ updates/20210927.js
@@ -1,10 +1,9 @@
-import Image from 'next/image'
 import page1 from '/public/images/20210927-page1.jpg'
 import page2 from '/public/images/20210927-page2.jpg'
 
 export default {
   "title": "",
-  "text": <div>Wrote up the basic model structure and page layout in my notebook. Also started the basics of a <a href="https://github.com/davemcg3/car-mx/projects/1">github project</a>.<br /><Image src={page1} alt="Writeout of the models and design layout, page 1" width={400} height={600} /><Image src={page2} alt="Writeout of the models and design layout, page 2" width={400} height={600} /></div>,
+  "text": <div>Wrote up the basic model structure and page layout in my notebook. Also started the basics of a <a href="https://github.com/davemcg3/car-mx/projects/1">github project</a>.<br /><img src={page1} alt="Writeout of the models and design layout, page 1" width={400} height={600} /><img src={page2} alt="Writeout of the models and design layout, page 2" width={400} height={600} /></div>,
   "author": "davemcg3",
   "date": "20210927"
 }
